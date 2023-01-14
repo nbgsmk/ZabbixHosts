b222d6d9e7e73428113532f504f783d5a34da5f0
peca745g2 <60575976+nbgsmk@users.noreply.github.com>
2023-01-14 05:06:03 +0100

    SNMPv2 za agregate

--- src/main/java/cc/kostic/zabbixhosts/metadata/Templejt.java
+++ src/main/java/cc/kostic/zabbixhosts/metadata/Templejt.java
@@ -2,8 +2,8 @@ package cc.kostic.zabbixhosts.metadata;
 
 public enum Templejt {
 	
-	tpl_3G("tpl_ICMP Ping 12h"),
-	tpl_Agregat("tpl_Agregat"),
+	tpl_3G("tpl_ICMP Ping 12h"),		// tpl_3G ok
+	tpl_Agregat("tpl_Agregat"),		// tpl_Agreat ok - scada
 	
 	tpl_Default("tpl_Default"),
 	
